84f166995048f0e122bf88a1ae6b797f07309092
jett <96192323+jertgfg@users.noreply.github.com>
2023-12-08 05:41:41 -0800

    Add files via upload

---
+++ Form11.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace c__mrthods_and_event_handlers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOperand1 = new System.Windows.Forms.Label();
+            this.lblOperator = new System.Windows.Forms.Label();
+            this.lblOperand2 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtOperand1 = new System.Windows.Forms.TextBox();
+            this.txtOperator = new System.Windows.Forms.TextBox();
+            this.txtOperand2 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOperand1
+            // 
+            this.lblOperand1.AutoSize = true;
+            this.lblOperand1.Location = new System.Drawing.Point(54, 45);
+            this.lblOperand1.Name = "lblOperand1";
+            this.lblOperand1.Size = new System.Drawing.Size(60, 13);
+            this.lblOperand1.TabIndex = 0;
+            this.lblOperand1.Text = "Operand 1:";
+            // 
+            // lblOperator
+            // 
+            this.lblOperator.AutoSize = true;
+            this.lblOperator.Location = new System.Drawing.Point(54, 92);
+            this.lblOperator.Name = "lblOperator";
+            this.lblOperator.Size = new System.Drawing.Size(51, 13);
+            this.lblOperator.TabIndex = 1;
+            this.lblOperator.Text = "Operator:";
+            // 
+            // lblOperand2
+            // 
+            this.lblOperand2.AutoSize = true;
+            this.lblOperand2.Location = new System.Drawing.Point(54, 141);
+            this.lblOperand2.Name = "lblOperand2";
+            this.lblOperand2.Size = new System.Drawing.Size(60, 13);
+            this.lblOperand2.TabIndex = 2;
+            this.lblOperand2.Text = "Operand 2:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(55, 185);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(40, 13);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "Result:";
+            // 
+            // txtOperand1
+            // 
+            this.txtOperand1.Location = new System.Drawing.Point(161, 38);
+            this.txtOperand1.Name = "txtOperand1";
+            this.txtOperand1.Size = new System.Drawing.Size(100, 20);
+            this.txtOperand1.TabIndex = 4;
+            this.txtOperand1.TextChanged += new System.EventHandler(this.ClearResult);
+            // 
+            // txtOperator
+            // 
+            this.txtOperator.Location = new System.Drawing.Point(161, 89);
+            this.txtOperator.Name = "txtOperator";
+            this.txtOperator.Size = new System.Drawing.Size(32, 20);
+            this.txtOperator.TabIndex = 5;
+            this.txtOperator.TextChanged += new System.EventHandler(this.ClearResult);
+            // 
+            // txtOperand2
+            // 
+            this.txtOperand2.Location = new System.Drawing.Point(161, 134);
+            this.txtOperand2.Name = "txtOperand2";
+            this.txtOperand2.Size = new System.Drawing.Size(100, 20);
+            this.txtOperand2.TabIndex = 6;
+            this.txtOperand2.TextChanged += new System.EventHandler(this.ClearResult);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(161, 178);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 7;
+            this.txtResult.TextChanged += new System.EventHandler(this.ClearResult);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(57, 215);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(152, 215);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(300, 250);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtOperand2);
+            this.Controls.Add(this.txtOperator);
+            this.Controls.Add(this.txtOperand1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblOperand2);
+            this.Controls.Add(this.lblOperator);
+            this.Controls.Add(this.lblOperand1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOperand1;
+        private System.Windows.Forms.Label lblOperator;
+        private System.Windows.Forms.Label lblOperand2;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtOperand1;
+        private System.Windows.Forms.TextBox txtOperator;
+        private System.Windows.Forms.TextBox txtOperand2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form11.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//
+// Isaac Ribero / Jett Conlin / Asgton Sypik
+// 12/7/2023
+// Simple Calculator
+//
+
+namespace c__mrthods_and_event_handlers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal Operand1;
+            decimal Operand2;
+            string Operator = txtOperator.Text;
+            decimal Result = 0;
+            if (decimal.TryParse(txtOperand1.Text, out Operand1))
+            {
+                if (decimal.TryParse(txtOperand2.Text, out Operand2))
+                {
+                    if (Operator == "+" || Operator == "-" || Operator == "*" || Operator == "/")
+                    {
+                        Result = Calculate(Operand1, Operand2, Operator);
+                        txtResult.Text = Result.ToString();
+                        txtOperand1.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid Number");
+                    }
+                  
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Number");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid Number");
+            }
+        
+        }
+        private decimal Calculate(decimal Operand1, decimal Operand2, string Operator)
+        {
+            decimal Result;
+
+            if (Operator == "+")
+            {
+                Result = Operand1 + Operand2;
+            }
+            else if (Operator == "-")
+            {
+                Result = Operand1 - Operand2;
+            }
+            else if (Operator == "*")
+            {
+                Result = Operand1 * Operand2;
+            }
+            else 
+            {
+                Result = Operand1 / Operand2;
+            }
+
+            return Result;
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = "";
+        }
+
+        private void ClearResult(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
